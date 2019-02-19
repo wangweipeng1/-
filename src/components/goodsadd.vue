28186560d4a4884e2c9d4bd6e57f97a5ed9a41bb
wwp <819795009@qq.com>
2019-02-19 23:01:30 +0800

    小调整

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -46,7 +46,19 @@
             <el-input v-model="item.attr_vals"></el-input>
           </el-form-item>
         </el-tab-pane>
-        <el-tab-pane name="4" label="商品图片"></el-tab-pane>
+        <el-tab-pane name="4" label="商品图片">
+          <el-form-item label="添加图片">
+            <el-upload
+              action="https://jsonplaceholder.typicode.com/posts/"
+              :on-success="handleSuccess"
+              :on-remove="handleRemove"
+              :file-list="fileList2"
+              list-type="picture"
+            >
+              <el-button size="small" type="primary">点击上传</el-button>
+            </el-upload>
+          </el-form-item>
+        </el-tab-pane>
         <el-tab-pane name="5" label="商品内容"></el-tab-pane>
       </el-tabs>
     </el-form>
@@ -76,13 +88,20 @@ export default {
         // children: "children"
       },
       arrDy: [],
-      arrStatic: []
+      arrStatic: [],
+      fileList2: []
     };
   },
   created() {
     this.getGoodsCate();
   },
   methods: {
+    handleSuccess(file, fileList) {
+      console.log(file);
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
     async changeTab() {
       if ((this.active === "2") | (this.active === "3")) {
         if (this.selectedOptions.length !== 3) {
