6bd16dbc548e4427482c3b2922c90f0d69c99135
wwp <819795009@qq.com>
2019-02-19 22:43:56 +0800

    商品属性完成

--- src/components/goodsadd.vue
+++ src/components/goodsadd.vue
@@ -41,7 +41,11 @@
             </el-checkbox-group>
           </el-form-item>
         </el-tab-pane>
-        <el-tab-pane name="3" label="商品属性"></el-tab-pane>
+        <el-tab-pane name="3" label="商品属性">
+          <el-form-item :label="item.attr_name" v-for="(item, i) in arrStatic" :key="item.attr_id">
+            <el-input v-model="item.attr_vals"></el-input>
+          </el-form-item>
+        </el-tab-pane>
         <el-tab-pane name="4" label="商品图片"></el-tab-pane>
         <el-tab-pane name="5" label="商品内容"></el-tab-pane>
       </el-tabs>
@@ -80,42 +84,44 @@ export default {
   },
   methods: {
     async changeTab() {
-      if (this.active === "2" | this.active === '3') {
+      if ((this.active === "2") | (this.active === "3")) {
         if (this.selectedOptions.length !== 3) {
           this.$message.error("请先选择三级分类!");
           return;
         }
-        if (this.active === '2') {
-            const res = await this.$http.get(
-              `categories/${this.selectedOptions[2]}/attributes?sel=many`
-            );
-            console.log(res, "获取动态分类参数列表");
-            const {
-              meta: { status, msg },
-              data
-            } = res.data;
-            if (status === 200) {
-              this.arrDy = data
-              this.arrDy.forEach((item) => {
-                  item.attr_vals = item.attr_vals.trim().length === 0 ? [] : item.attr_vals.trim().split(',')
-              })
-              console.log(this.arrDy, "动态分类参数");
-            }
-
+        if (this.active === "2") {
+          const res = await this.$http.get(
+            `categories/${this.selectedOptions[2]}/attributes?sel=many`
+          );
+          console.log(res, "获取动态分类参数列表");
+          const {
+            meta: { status, msg },
+            data
+          } = res.data;
+          if (status === 200) {
+            this.arrDy = data;
+            this.arrDy.forEach(item => {
+              item.attr_vals =
+                item.attr_vals.trim().length === 0
+                  ? []
+                  : item.attr_vals.trim().split(",");
+            });
+            console.log(this.arrDy, "动态分类参数");
+          }
         }
-        if (this.active === '3') {
-            const res = await this.$http.get(
-              `categories/${this.selectedOptions[2]}/attributes?sel=only`
-            );
-            console.log(res, "获取静态分类参数列表");
-            const {
-              meta: { status, msg },
-              data
-            } = res.data;
-            if (status === 200) {
-              this.arrStatic = data
-              console.log(this.arrStatic, "静态分类参数");
-            }
+        if (this.active === "3") {
+          const res = await this.$http.get(
+            `categories/${this.selectedOptions[2]}/attributes?sel=only`
+          );
+          console.log(res, "获取静态分类参数列表");
+          const {
+            meta: { status, msg },
+            data
+          } = res.data;
+          if (status === 200) {
+            this.arrStatic = data;
+            console.log(this.arrStatic, "静态分类参数");
+          }
         }
       }
     },
